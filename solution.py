4979ede5605ca693dcbed67d9eabeb83e5e4762f
tomasmartins77 <tomasmartins77@tecnico.ulisboa.pt>
2023-09-14 11:19:01 +0100

    nao sei a melhor forma de guardar o grafo logo nao consegui fazer tod

--- solution.py
+++ solution.py
@@ -1,56 +1,74 @@
-import math
-import numpy as np
-
-
 class FleetProblem(object):
+
     def __init__(self):
-        self.fh = None
         self.sol = None
         self.P = None
         self.R = None
-        self.td = None
-        self.t = None
-        self.Tod = None
-        self.matriz = None
         self.V = None
+        self.graph = []
+        self.requests = []
+        self.vehicles = []
 
     def load(self, fh):
         current_mode = None
-        i = 0
         with open(fh, 'r') as f:
             for line in f:
                 words = line.split()
+
+                if not words:
+                    continue  # Skip empty lines
+
                 if words[0] == 'P':
-                    current_line = 0
                     self.P = int(words[1])
-                    self.matriz = [[0 for _ in range(self.P)] for _ in range(self.P)]
                     current_mode = 'P'
-                    print(self.P)
+
                 elif words[0] == 'R':
                     self.R = int(words[1])
                     current_mode = 'R'
+
                 elif words[0] == 'V':
                     self.V = int(words[1])
                     current_mode = 'V'
+
                 else:
                     if current_mode == 'P':
-                        self.matriz[i] = words
-                        i += 1
+                        row = list(map(int, words))
+                        self.graph.append(row)
                     elif current_mode == 'R':
-                        self.t.append([int(words[0]), int(words[1])])
+                        request = list(map(int, words))
+                        self.requests.append(request)
                     elif current_mode == 'V':
-                        self.Tod.append([int(words[0]), int(words[1])])
+                        vehicle = list(map(int, words))
+                        self.vehicles.append(vehicle)
                     else:
                         raise Exception('Invalid mode')
-
-            print(self.matriz)
+        print(self.graph)
+        print(self.requests)
         pass
 
     def cost(self, sol):
+        td = 0
+        t = 0
+        tod = 0
+        final_cost = 0
+        for action in sol:
+            if action[0] == 'Dropoff':
+                td = action[3]
+                t = self.requests[action[2] - 1][0]
+                tod = "?"
 
+            final_cost += td - t - tod
+        print(final_cost)
         pass
 
 
 if __name__ == '__main__':
+    sols = [('Pickup', 0, 3, 30.0), ('Pickup', 1, 4, 25.0), ('Pickup', 1, 0, 25.0),
+           ('Dropoff', 1, 4, 75.0), ('Dropoff', 1, 0, 75.0), ('Pickup', 0, 2, 30.0),
+           ('Dropoff', 0, 3, 80.0), ('Pickup', 0, 1, 80.0), ('Dropoff', 0, 1, 140.0),
+           ('Dropoff', 0, 2, 140.0)]
+
     fp = FleetProblem()
-    fp.load('test.txt')
+    file_path = 'test.txt'
+    fp.load(file_path)
+    fp.cost(sols)
