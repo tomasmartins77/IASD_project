241fe8b5a6e90703b036a4689c49901cf95f3153
tomasmartins77 <tomasmartins77@tecnico.ulisboa.pt>
2023-09-19 11:31:46 +0100

    small changes

--- solution.py
+++ solution.py
@@ -1,55 +1,55 @@
 #!/usr/bin/env python3
 
-import io
+import search
 
 
-class FleetProblem:
+# Define a class for solving the Fleet Problem
+class FleetProblem(search.Problem):
     def __init__(self):
-        self.sol = None
-        self.graph = None
-        self.requests = None
-        self.vehicles = None
+        # Initialize instance variables
+        self.sol = None  # Store the solution
+        self.graph = None  # Store the graph data
+        self.requests = None  # Store the request data
+        self.vehicles = None  # Store the vehicle data
 
+    # Load data from a file into the FleetProblem instance
     def load(self, file_content):
-        current_mode = None
-        row = 0
+        current_mode = None  # Track the current mode (P, R, or V)
+        row = 0  # Track the current row being processed
         P = 0
 
         for line in file_content:
-            words = line.split()
+            words = line.split()  # Split the line into words
 
             if not words or words[0].startswith('#'):
                 continue  # Skip empty lines and comments
 
             if words[0] == 'P':
-                P = int(words[1])
-                self.graph = Graph(P)
-                current_mode = 'P'
+                P = int(words[1])  # Get the number of vertices
+                self.graph = Graph(P)  # Initialize the graph
+                current_mode = 'P'  # Set the mode to process graph data
             elif words[0] == 'R':
-                self.requests = Requests(int(words[1]))
-                current_mode = 'R'
+                self.requests = Requests(int(words[1]))  # Initialize requests
+                current_mode = 'R'  # Set the mode to process request data
             elif words[0] == 'V':
-                self.vehicles = Vehicles(int(words[1]))
-                current_mode = 'V'
+                self.vehicles = Vehicles(int(words[1]))  # Initialize vehicles
+                current_mode = 'V'  # Set the mode to process vehicle data
             else:
                 if current_mode == 'P':
-                    col = 1 - P
+                    col = 1 - P  # Initialize column index for graph data
                     for weight in words:
-                        self.graph.add_edge(row, col, float(weight))
+                        self.graph.add_edge(row, col, float(weight))  # Add edge data to the graph
                         col += 1
                     row += 1
                     P -= 1
                 elif current_mode == 'R':
-                    self.requests.add_request(words)
+                    self.requests.add_request(words)  # Add request data
                 elif current_mode == 'V':
-                    self.vehicles.add_vehicle(words)
+                    self.vehicles.add_vehicle(words)  # Add vehicle data
                 else:
-                    raise Exception('Invalid mode')
+                    raise Exception('Invalid mode')  # Handle invalid mode
 
-        self.graph.print_graph()
-        self.requests.print_requests()
-        self.vehicles.print_vehicles()
-
+    # Calculate the cost of a solution
     def cost(self, sol):
         total_cost = 0
         for action in sol:
@@ -58,25 +58,29 @@ class FleetProblem:
                 td = action[3]
                 t = request[0]
                 Tod = self.graph.get_edge(request[1], request[2])
-                total_cost += td - t - Tod
+                total_cost += td - t - Tod  # Calculate cost based on the solution
 
         return total_cost
 
 
+# Define a class for representing a graph
 class Graph:
     def __init__(self, num_vertices, directed=False):
         self.num_vertices = num_vertices
         self.directed = directed
-        self.graph = [[0] * num_vertices for _ in range(num_vertices)]
+        self.graph = [[0] * num_vertices for _ in range(num_vertices)]  # Initialize adjacency matrix
 
+    # Add an edge to the graph
     def add_edge(self, u, v, w):
         self.graph[u][v] = w
         if not self.directed:
             self.graph[v][u] = w
 
+    # Get the weight of an edge
     def get_edge(self, u, v):
         return self.graph[u][v]
 
+    # Print the graph
     def print_graph(self):
         print('Graph:')
         for row in self.graph:
@@ -84,11 +88,13 @@ class Graph:
         print()
 
 
+# Define a class for representing requests
 class Requests:
     def __init__(self, num_requests):
         self.num_requests = num_requests
         self.requests = []
 
+    # Add a request to the list of requests
     def add_request(self, words):
         value = 0
         request = []
@@ -102,9 +108,11 @@ class Requests:
             request.append(value)
         self.requests.append(request)
 
+    # Get a request by index
     def get_request(self, index):
         return self.requests[index]
 
+    # Print the list of requests
     def print_requests(self):
         print('Requests:')
         for request in self.requests:
@@ -112,18 +120,22 @@ class Requests:
         print()
 
 
+# Define a class for representing vehicles
 class Vehicles:
     def __init__(self, num_vehicles):
         self.num_vehicles = num_vehicles
         self.vehicles = []
 
+    # Add a vehicle to the list of vehicles
     def add_vehicle(self, words):
         vehicle = int(words[0])
         self.vehicles.append(vehicle)
 
+    # Get a vehicle by index
     def get_vehicle(self, index):
         return self.vehicles[index]
 
+    # Print the list of vehicles
     def print_vehicles(self):
         print('Vehicles:')
         for vehicle in self.vehicles:
